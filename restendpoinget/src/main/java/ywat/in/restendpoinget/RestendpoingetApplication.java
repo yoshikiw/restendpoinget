52b73614f4e7dc616a104ae1c5b17588d8e70230
yoshikiw <yoshiki.w7@gmail.com>
2023-07-03 23:47:40 +0900

    Initial commit

---
+++ restendpoinget/src/main/java/ywat/in/restendpoinget/RestendpoingetApplication.java
@@ -0,0 +1,13 @@
+package ywat.in.restendpoinget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestendpoingetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestendpoingetApplication.class, args);
+	}
+
+}
---
+++ restendpoinget/src/test/java/ywat/in/restendpoinget/RestendpoingetApplicationTests.java
@@ -0,0 +1,13 @@
+package ywat.in.restendpoinget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestendpoingetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
